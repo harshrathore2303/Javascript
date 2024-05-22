254e4ade33f253d7a5bfb97f1578156567c6d20f
Harshit Singh Rathore <11648840+rathoregroup@users.noreply.github.com>
2024-05-22 16:12:19 +0530

    Add files via upload

---
+++ 02_operators/operator.js
@@ -0,0 +1,25 @@
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+console.log(ageJonas * 2, ageJonas / 2);
+console.log(ageSarah ** 2, ageSarah / 2, ageSarah + ageJonas);
+
+const firstName = 'Kane';
+const lastName = 'Winslet';
+console.log(firstName + ' ' + lastName);
+
+
+//AssignmentOperators
+let x = 10 + 5; //15
+x += 10; //x = x + 10
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageSarah); //>, <, <=, >=, ==, ===
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2019);

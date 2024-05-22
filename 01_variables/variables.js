d79336c0911250c013edcc9466a9c202f2552200
Harshit Singh Rathore <11648840+rathoregroup@users.noreply.github.com>
2024-05-22 15:24:12 +0530

    Add files via upload

---
+++ 01_variables/variables.js
@@ -0,0 +1,36 @@
+console.log(40 + 8 - 2 + 6);
+console.log("--------");
+console.log("|      |");
+console.log("|      |");
+console.log("|      |");
+console.log("|      |");
+console.log("--------");
+console.log("Nice");
+let js = "amazing";
+console.log(js);
+let $name = "Harsh";
+console.log($name);
+let PI = 3.1415;
+console.log(PI);
+
+//value
+//datatypes
+//number, string, boolean, undefined, null, symbol, bigint
+let bool = true;
+bool = false;
+console.log(typeof bool);
+let year;
+console.log(typeof year);
+console.log(typeof null);
+
+const birthYear = 2002;
+console.log(birthYear);
+// connot be declared empty
+// const eyeColor;
+//cannot be mutate
+
+var job = "programmer";
+job = "teacher"
+
+lastName = "Rathore";
+console.log(lastName)

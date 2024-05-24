ad1a8f14a257232ee37bbb3c4fc1fbd1c48ffe49
Harshit Singh Rathore <11648840+rathoregroup@users.noreply.github.com>
2024-05-24 14:26:29 +0530

    Update conditionals.js

--- 04_conditionals/conditionals.js
+++ 04_conditionals/conditionals.js
@@ -14,3 +14,9 @@ if (birthYear > 2000){
     century = 2;
 }
 console.log(century);
+
+// ternaryOperator
+const newAge = 23;
+newAge >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+const drink = newAge >= 18 ? 'wine' : 'water';
+console.log(drink);

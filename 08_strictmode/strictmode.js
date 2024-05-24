d1ba753a2666a82fa1de0baacc3a98449d7eebd8
Harshit Singh Rathore <11648840+rathoregroup@users.noreply.github.com>
2024-05-24 15:40:43 +0530

    Add files via upload

---
+++ 08_strictmode/strictmode.js
@@ -0,0 +1,12 @@
+//more secure code and error can be detected easily
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('i can drive:D');
+
+// const interface = 'Audio';
+// const private = 5846;
+// const if = 23;
